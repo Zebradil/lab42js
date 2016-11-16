b3126676161fcedc1cc3d640f1b430d381c57449
German Lashevich <german.lashevich@tatar.ru>
2016-11-16 14:37:16 +0300

    Remove duplicate

--- 05/demo-07.js
+++ 05/demo-07.js
@@ -30,22 +30,6 @@
         user.sayHi();
     }, 1000);
 
-})();
-
-(function () {
-    "use strict";
-
-    var user = {
-        firstName: "Вася",
-        sayHi: function () {
-            console.log(this.firstName);
-        }
-    };
-
-    setTimeout(function () {
-        user.sayHi();
-    }, 1000);
-
     user = null;
 
 })();
